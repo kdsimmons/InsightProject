7fc690894c3a69120c2fa4cc81d87ba888cb84a8
Kristy Simmons <kristina.d.simmons@gmail.com>
2015-07-15 20:44:51 -0400

    Fixed broken links in output.

--- app/views.py
+++ app/views.py
@@ -18,59 +18,6 @@ def index():
         title = 'Home', user = { 'nickname': 'Miguel' },
         )
 
-@app.route('/db')
-def cities_page():
-    with citydb: 
-        cur = citydb.cursor()
-        cur.execute("SELECT Name FROM City LIMIT 15;")
-        query_results = cur.fetchall()
-    cities = ""
-    for result in query_results:
-        cities += result[0]
-        cities += "<br>"
-    return cities
-
-@app.route("/db_fancy")
-def cities_page_fancy():
-    with citydb:
-        cur = citydb.cursor()
-        cur.execute("SELECT Name, CountryCode, Population FROM City ORDER BY Population LIMIT 15;")
-
-        query_results = cur.fetchall()
-    cities = []
-    for result in query_results:
-        cities.append(dict(name=result[0], country=result[1], population=result[2]))
-    return render_template('cities.html', cities=cities)
-
-"""
-@app.route('/input')
-def cities_input():
-    return render_template("input.html")
-
-@app.route('/output')
-def cities_output():
-    #pull 'ID' from input field and store it
-    city = request.args.get('ID')
-
-    with db:
-        cur = db.cursor()
-        #just select the city from the world_innodb that the user inputs
-        cur.execute("SELECT Name, CountryCode,  Population FROM City WHERE Name='%s';" % city)
-        query_results = cur.fetchall()
-
-    cities = []
-    for result in query_results:
-        cities.append(dict(name=result[0], country=result[1], population=result[2]))
-    #call a function from a_Model package. note we are only pulling one result in the query
-    if len(cities) > 0:
-        pop_input = cities[0]['population']
-        the_result = ModelIt(city, pop_input)
-    else:
-        the_result = 'unknown because you entered a city not included in the database'
-
-    return render_template("output.html", cities = cities, the_result = the_result)
-"""
-
 @app.route('/input')
 def charity_input():
     return render_template("charity_input.html")
@@ -101,7 +48,7 @@ def charity_output():
         the_result = 'unknown because you entered a city not included in the database'
     """
 
-    the_result = str(0) #placeholder for now - the_result isn't used in current output template
+    the_result = str(0) #placeholder for now - the_result is only used for debugging
   
     return render_template("charity_output.html", charities = charities, the_result = the_result, custom_error = '')
 
